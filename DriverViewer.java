8fbdfee130634b47d2fab7d242e91880f7261450
Henry <henrybecker1014@gmail.com>
2024-06-27 15:54:01 -0400

    Now we have a fully functioning pyramid that spins both in x, y and z

--- DriverViewer.java
+++ DriverViewer.java
@@ -2,6 +2,10 @@ package java3D;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.awt.geom.*;
+// import java.awt.image.BufferedImage;
 
 public class DriverViewer{
 
@@ -12,12 +16,20 @@ public class DriverViewer{
 
 
         // Slider that controls moving in a horizontal Direction
-        JSlider headingSlider = new JSlider(0, 360, 180);
+        JSlider headingSlider = new JSlider(-180, 180, 0);
         pane.add(headingSlider, BorderLayout.SOUTH);
 
         // slider to control vertical direction 
         JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90,90, 0);
         pane.add(pitchSlider, BorderLayout.EAST);
+
+        // // slider to control roll
+        // JSlider rollSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        // pane.add(rollSlider, BorderLayout.WEST);
+
+        // // slider to control FoV
+        // JSlider FoVSlider = new JSlider(1, 179, 60);
+        // pane.add(FoVSlider, BorderLayout.NORTH);
 
         // panel to display render results
         JPanel renderPanel = new JPanel(){
@@ -25,14 +37,122 @@ public class DriverViewer{
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
-            
-            
+                
+
+                List<Triangle> tris = new ArrayList<Triangle>();
+                tris.add(new Triangle(new Vertex(100, 100, 100), 
+                                      new Vertex(-100, -100, 100), 
+                                      new Vertex(-100, 100, -100), 
+                                      Color.WHITE));
+                tris.add(new Triangle(new Vertex(100, 100, 100), 
+                                      new Vertex(-100, -100, 100), 
+                                      new Vertex(100, -100, -100), 
+                                      Color.RED));
+                tris.add(new Triangle(new Vertex(-100, 100, -100), 
+                                      new Vertex(100, -100, -100), 
+                                      new Vertex(100, 100, 100), 
+                                      Color.GREEN));
+                tris.add(new Triangle(new Vertex(-100, 100, -100), 
+                                      new Vertex(100, -100, -100), 
+                                      new Vertex(-100, -100, 100), 
+                                      Color.BLUE));
+
+                double heading = Math.toRadians(headingSlider.getValue());
+                Matrix3 transform = new Matrix3(new double[] {
+                        Math.cos(heading), 0, -Math.sin(heading),
+                        0, 1, 0,
+                        Math.sin(heading), 0, Math.cos(heading)
+                    });
+
+                Matrix3 headingTransform = new Matrix3 (new double[] {
+                    Math.cos(heading), 0, Math.sin(heading),
+                    0, 1, 0,
+                    -Math.sin(heading), 0, Math.cos(heading)
+                });
+                double pitch = Math.toRadians(pitchSlider.getValue());
+                Matrix3 pitchTransform = new Matrix3(new double[] {
+                        1, 0, 0,
+                        0, Math.cos(pitch), Math.sin(pitch),
+                        0, -Math.sin(pitch), Math.cos(pitch)
+                    });
+                transform = headingTransform.multiply(pitchTransform);
+                
+                // Rasterization via barycentic coordinates isnt as efficient as ahrdware raterization
+                
+                
+
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                g2.setColor(Color.WHITE);
+                    for (Triangle t : tris) {
+                        Vertex v1 = transform.transform(t.v1);
+                        Vertex v2 = transform.transform(t.v2);
+                        Vertex v3 = transform.transform(t.v3);
+                        Path2D path = new Path2D.Double();
+                        path.moveTo(v1.x, v1.y);
+                        path.lineTo(v2.x, v2.y);
+                        path.lineTo(v3.x, v3.y);
+                        path.closePath();
+                        g2.draw(path);
+                    }
                 }
+                
             }; 
         pane.add(renderPanel, BorderLayout.CENTER);
+
+        headingSlider.addChangeListener(e -> renderPanel.repaint());
+        pitchSlider.addChangeListener(e -> renderPanel.repaint());
         
         frame.setSize(400, 400);
         frame.setVisible(true);
 
     }
+}
+
+class Vertex{
+    double x,y,z;
+    Vertex(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+class Triangle{
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Color color;
+    Triangle(Vertex v1, Vertex v2, Vertex v3, Color color){
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+}
+
+class Matrix3{
+    double[] values;
+    Matrix3(double[] values){
+        this.values = values;
+    }
+    Matrix3 multiply(Matrix3 other){
+    double[] result = new double[9];
+        for (int row = 0; row < 3; row++){
+            for (int col = 0; col < 3; col++){
+                for (int i = 0; i < 3; i++){
+                    result[row * 3 + col] += 
+                        this.values[row * 3 + i] * other.values[i * 3 + col];
+                }
+            }
+        }
+        return new Matrix3(result);
+    }
+    Vertex transform(Vertex in){
+        return new Vertex(
+            in.x * values[0] + in.y * values[3] + in.z * values[6],
+            in.x * values[1] + in.y * values[4] + in.z * values[7],
+            in.x * values[2] + in.y * values[5] + in.z * values[8]
+        );
+    }    
+
 }
