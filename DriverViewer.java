8f73937829a2420fbe10324088de4df0a1d76e43
Henry <henrybecker1014@gmail.com>
2024-06-27 10:00:39 -0400

    Here is the code to generate a black box with a vertical and horizontal slider.

---
+++ DriverViewer.java
@@ -0,0 +1,38 @@
+package java3D;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DriverViewer{
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
+
+
+        // Slider that controls moving in a horizontal Direction
+        JSlider headingSlider = new JSlider(0, 360, 180);
+        pane.add(headingSlider, BorderLayout.SOUTH);
+
+        // slider to control vertical direction 
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90,90, 0);
+        pane.add(pitchSlider, BorderLayout.EAST);
+
+        // panel to display render results
+        JPanel renderPanel = new JPanel(){
+            public void paintComponent(Graphics g){
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setColor(Color.BLACK);
+                g2.fillRect(0, 0, getWidth(), getHeight());
+            
+            
+                }
+            }; 
+        pane.add(renderPanel, BorderLayout.CENTER);
+        
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+
+    }
+}
